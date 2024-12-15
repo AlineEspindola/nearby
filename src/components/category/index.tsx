23567c505686a7917db539c5ecf944601fb420eb
AlineEspindola <aline.ae@aluno.ifsc.edu.br>
2024-12-15 12:53:19 -0300

    fix: Permite clicar nas últimas categorias

--- src/components/category/index.tsx
+++ src/components/category/index.tsx
@@ -3,8 +3,10 @@ import { Text, Pressable, PressableProps } from "react-native"
 import { s } from "./styles"
 import { categoriesIcons } from "@/utils/categories-icons"
 import { colors } from "@/styles/theme"
+import { TouchableWithoutFeedback } from "@gorhom/bottom-sheet"
+import { TouchableWithoutFeedbackProps } from "react-native"
 
-type Props = PressableProps & {
+type Props = TouchableWithoutFeedbackProps & {
   name: string
   iconId: string
   isSelected?: boolean
@@ -14,11 +16,11 @@ export function Category({ name, iconId, isSelected = false, ...rest }: Props) {
   const Icon = categoriesIcons[iconId]
 
   return (
-    <Pressable style={[s.container, isSelected && s.containerSelected]} {...rest}>
+    <TouchableWithoutFeedback style={[s.container, isSelected && s.containerSelected]} {...rest}>
       <Icon size={16} color={colors.gray[isSelected ? 100 : 400]} />
       <Text style={[s.name, isSelected && s.nameSelected]}>
         {name}
       </Text>
-    </Pressable>
+    </TouchableWithoutFeedback>
   )
 }
